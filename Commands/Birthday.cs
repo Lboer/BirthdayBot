786a037186b0e82a77e6801f4fd553289c825b09
Luuk de Boer <porthosmine@gmail.com>
2020-12-30 15:59:43 +0100

    Default bot setup

---
+++ Commands/Birthday.cs
@@ -0,0 +1,26 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berdthday_Bot.Commands
+{
+    class Birthday : BaseCommandModule
+    {
+        public string Mention { get; }
+        [Command("congratulate")]
+        [Description("Congratulate user on birthday")]
+        public async Task congratulate(CommandContext ctx, DiscordMember member)
+        {
+            await ctx.Channel
+                .SendMessageAsync("Happy Birthday " + member.Mention + "!").ConfigureAwait(false);
+            await ctx.Channel
+                .SendMessageAsync("https://www.youtube.com/watch?v=XtIBHfOdyX0").ConfigureAwait(false);
+        }
+    }
+}
