0da1ff4c4be68fae7ea99752e5acd69bc95344d6
Luuk de Boer <porthosmine@gmail.com>
2020-12-31 13:13:55 +0100

    Birthday model

--- Commands/Birthday.cs
+++ Commands/Birthday.cs
@@ -1,4 +1,5 @@
-﻿using DSharpPlus.CommandsNext;
+﻿using Berdthday_Bot.Models;
+using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using DSharpPlus.Interactivity.Extensions;
@@ -13,6 +14,7 @@ namespace Berdthday_Bot.Commands
     class Birthday : BaseCommandModule
     {
         public string Mention { get; }
+        private List<BirthdayList> birthdays = new List<BirthdayList>();
         [Command("congratulate")]
         [Description("Congratulate user on birthday")]
         public async Task congratulate(CommandContext ctx, DiscordMember member)
@@ -21,6 +23,18 @@ namespace Berdthday_Bot.Commands
                 .SendMessageAsync("Happy Birthday " + member.Mention + "!").ConfigureAwait(false);
             await ctx.Channel
                 .SendMessageAsync("https://www.youtube.com/watch?v=XtIBHfOdyX0").ConfigureAwait(false);
+        }
+
+        [Command("add")]
+        [Description("Add a birthday")]
+        public async Task add(CommandContext ctx, DiscordMember member, string birthday)
+        {
+            await ctx.Channel.SendMessageAsync("Adding!");
+            var userBirthday = new BirthdayList();
+            userBirthday.AddBirthday(member.ToString(), DateTime.Parse(birthday));
+            await ctx.Channel.SendMessageAsync(userBirthday.username.ToString() + userBirthday.birthday.ToString());
+            birthdays.Add(userBirthday);
+            await ctx.Channel.SendMessageAsync("Added!");
         }
     }
 }
---
+++ Models/BirthdayList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Berdthday_Bot.Models
+{
+    public class BirthdayList
+    {
+        public string username { get; set; }
+        public DateTime birthday { get; set; }
+
+        public void AddBirthday(string User, DateTime Birthday)
+        {
+            username = User;
+            birthday = Birthday;
+        }
+    }
+}
